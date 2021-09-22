7a168f928c458acd534c6bfd88fed52dd6660e68
drivelikejehu <pollyfactor@hotmail.com>
2021-09-22 10:08:26 -0400

    successfully deployed functions, addRequest

--- functions/index.js
+++ functions/index.js
@@ -1,22 +1,42 @@
 const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+admin.initializeApp();
 
-// http request 1
-
-exports.randomNumber = functions.https.onRequest((request, response) => {
-  const number = Math.round(Math.random() * 100);
-  console.log('random number: ', number);
-  response.send(number.toString());
+// auth trigger (new user signup)
+exports.newUserSignUp = functions.auth.user().onCreate((user) => {
+  // for background triggers you must return a value/promise
+  return admin.firestore().collection('users').doc(user.uid).set({
+    email: user.email,
+    upvotedOn: [],
+  });
 });
 
-// http request 1
-
-exports.toTheDojo = functions.https.onRequest((request, response) => {
-  response.redirect('https://www.thenetninja.co.uk');
+// auth trigger (user deleted)
+exports.userDeleted = functions.auth.user().onDelete((user) => {
+  const doc = admin.firestore().collection('users').doc(user.uid);
+  return doc.delete();
 });
 
-// http callable function
+// http callable functions (adding a request)
+// data we send to the function, data from the front end when we add a request
+// context contains information state of user aka user context
+// when we do a callable function we need to return something to the client
 
-exports.sayHello = functions.https.onCall((data, context) => {
-  const name = data.name;
-  return `hello ${name}`;
+exports.addRequest = functions.https.onCall((data, context) => {
+  if (!context.auth) {
+    throw new functions.https.HttpsError(
+        'unauthenticated',
+        'only authenticated users can add requests'
+    );
+  }
+  if (data.text.length > 30) {
+    throw new functions.https.HttpsError(
+        'invalid-argument',
+        'requests must be no more than 30 characters long'
+    );
+  }
+  admin.firestore().collection('requests').add({
+    text: data.text,
+    upVotes: 0,
+  });
 });
